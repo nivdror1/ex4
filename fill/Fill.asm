5f134324fb407523d7f39e66ba79f3027d600863
omribloch <omri.bloch@gmail.com>
2016-11-08 15:49:05 +0200

    first try

--- fill/Fill.asm
+++ fill/Fill.asm
@@ -12,3 +12,61 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+
+@pressed
+M = 0
+
+@24576
+D = M
+
+@WHITE
+D; JEQ
+
+(BLACK)
+@pressed
+M = 1
+
+@PROJ
+0;JMP
+
+(WHITE)
+@pressed
+M = 0
+
+@PROJ
+0;JMP
+
+
+(PROJ)
+@pressed
+D = M
+
+@16384      // A = 16384, the screen's top-left pixel
+D = M
+@i          // i = 16384
+M = D
+
+(LOOP)
+
+@pressed    // D = is key pressed?
+D = M
+
+@i          // insert pressed to the pixel
+A = M
+M = D
+
+@i
+A = M
+M = M + 1
+D = M
+
+@24384
+D = A - D
+@LOOP
+D, JNE
+
+
+@START
+0;JMP
