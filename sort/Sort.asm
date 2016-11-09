d77b448136a9e98a2c7718472ede9d6ec1e18485
nivdror1 <nivdror1@gmail.com>
2016-11-09 15:49:46 +0200

    sort

--- sort/Sort.asm
+++ sort/Sort.asm
@@ -1,21 +1,23 @@
-// sort the array which starts at R14 and the length of the array is located in R15.
+//input: R14 - the address of the start of the array
+         R15 - the length of the array
+//output: sort the array in descending order.
 // (R14, R15 refer to RAM[14], and RAM[15], respectively.)
 
 //pseudo code:
-  //set switch as R14
+  //check if the R15 input if less or equal to 1 then end the program
   // set n as the length current unsorted array
-  //for loop as long as the length of the array
-  // set i as zero
-  //set k as n
-  //for loop until it reaches n
-  // switch if necessary
-  // advance i
-  //end loop
+  //begin the outer loop(n>0)
+  //set k as n-1
+  //set cur_address as R14 and next_address as R14+1
+  //begin the inner loop (k>0)
+  // check if the next_address is bigger than cur_address than switch the value in the addresses
+  // decrease k by 1
+  // advance cur_address and next_address
+  //end the inner loop
   //decrease n by 1
-  //set switch as R14
-  //end loop
+  //end the outer loop
 
-@R15 // check if the input of the array length is less or equal to one
+@R15 // check if the input of the array length is less or equal to one if so end the program
 D=M-1
 @END
 D;JLE
@@ -25,11 +27,11 @@ D=M
 @n
 M=D
 
-(OUTER_LOOP)
+(OUTER_LOOP) //begin the outer loop if n>0
 @n
 D=M
-@END_OUTER_LOOP//outer loop
-D;JLE // A for loop over the array
+@END_OUTER_LOOP
+D;JLE // if n<=0 jump to END_OUTER_LOOP
 
 
 @n //set k as the current n
@@ -38,61 +40,61 @@ D=M
 M=D
 
 
-@R14 //set switch
+@R14 //set cur_address
 D=M
-@switch
+@cur_address
 M=D
-D=D+1 // set next
-@next
+D=D+1 // set next_address
+@next_address
 M=D
 
 
-(INNER_LOOP)
+(INNER_LOOP) //begin the inner loop if k>0
 @k
 D=M-1
 @END_INNER_LOOP
-D;JLE
+D;JLE // if k<=0 jump to END_INNER_LOOP
 
 
-@switch //if condition that continue if the switch is bigger than next
+@cur_address //an if condition that checks if next_address is bigger than cur_address
 A=M
 D=M
 @temp_arr
-M=D
-@next
+M=D //save the value that cur_address point at
+@next_address
 A=M
-D=M
+D=M // D register now contain the value that next_address point at
 @temp_arr
-D=D-M
+D=D-M // if the result is less or equal to zero than jump to END_IF
 @END_IF
 D;JLE
 
 // switch the values
-@next
+@next_address
 A=M
 D=M
 @temp
 M=D //temp contains next
-@switch
+@cur_address
 A=M
 D=M
-@next
+@next_address
 A=M
-M=D  //switch->next
+M=D  //cur_address->next_address
 @temp
 D=M
-@switch
+@cur_address
 A=M
-M=D //next->switch
+M=D //next_address->cur_address
 (END_IF)
 
 @k
 M=M-1
 
-@switch
+@cur_address
 M=M+1
 
-@next
+@next_address
 M=M+1
 
 
@@ -104,10 +106,6 @@ M=M+1
 @n
 M=M-1
 
-@R14 //reset switch as  R14 (the start of the array)
-D=M
-@switch
-M=D
 @OUTER_LOOP
 0;JMP
 
